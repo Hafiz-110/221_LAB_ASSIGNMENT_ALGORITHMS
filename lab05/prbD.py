b38db6dd9f38734cdb99634d575b71840a22ae3d
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-17 19:55:44 +0600

    Add files via upload

---
+++ lab05/prbD.py
@@ -0,0 +1,52 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def graph(ver, e):
+    g = [[] for _ in range(ver+1)]
+    for i in range(e):
+        u, v = map(int, input().split())
+        g[u].append(v)
+    for i in range(1, ver+1):
+        g[i].sort()
+
+    return g
+
+n, m, s, d, k = map(int, input().split())
+graphh = graph(n, m)
+
+def bfs(g, s, d, n):
+    distance = {i:float('inf') for i in range(1, n+1)}
+    parent = {i:-1 for i in range(1, n+1)}
+    distance[s] = 0
+    parent[s] = None
+    q = deque(); q.append(s)
+    while q:
+        u = q.popleft()
+        for v in g[u]:
+            if distance[v] > distance[u]+1:
+                distance[v] = distance[u]+1
+                parent[v] = u
+                q.append(v)
+
+    if distance[d] == float('inf'): return -1, []
+    else:
+        path = []
+        u = d
+        while u:
+            path.append(u)
+            u = parent[u]
+    path.reverse()
+    return len(path)-1, path
+
+len_path1, path1 = bfs(graphh, s, k, n)
+if len_path1 == -1:
+    print(-1); exit()
+
+len_path2, path2 = bfs(graphh, k, d, n)
+if len_path2 == -1:
+    print(-1); exit()
+
+full_path = path1+path2[1:]
+print(len(full_path)-1)
+print(*full_path)
