01276a9de2d88ac5f422461eb48991a8bfc8b4be
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-17 18:44:04 +0600

    Update prbC.py

--- lab05/prbC.py
+++ lab05/prbC.py
@@ -18,14 +18,12 @@ def graph(ver, e):
 n, m, s, d = map(int, input().split())
 graphh = graph(n, m)
 
-distance = {i:float('inf') for i in range(1, n+1)}
-parent = {i:-1 for i in range(1, n+1)}
-distance[s] = 0
-parent[s] = None
-
-q = deque(); q.append(s)
-
-def bfs():
+def bfs(s, d):
+    distance = {i:float('inf') for i in range(1, n+1)}
+    parent = {i:-1 for i in range(1, n+1)}
+    distance[s] = 0
+    parent[s] = None
+    q = deque(); q.append(s)
     while q:
         u = q.popleft()
         for v in graphh[u]:
@@ -33,15 +31,14 @@ def bfs():
                 distance[v] = distance[u]+1
                 parent[v] = u
                 q.append(v)
-bfs()
+    if distance[d] == float('inf'): print(-1)
+    else:
+        path = []
+        u = d
+        while u:
+            path.append(u)
+            u = parent[u]
+        print(len(path) - 1)
+        print(*reversed(path))
 
-# backtracking to source
-if distance[d] == float('inf'): print(-1)
-else:
-    path = []
-    u = d
-    while u:
-        path.append(u)
-        u = parent[u]
-    print(len(path) - 1)
-    print(*reversed(path))
+bfs(s, d)
