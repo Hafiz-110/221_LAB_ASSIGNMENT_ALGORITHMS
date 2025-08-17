96f42d0e18140f6c567dbe1213118fb59abd3ae0
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-17 22:57:35 +0600

    Add files via upload

---
+++ lab05/prbE.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def graph(ver, e):
+    g = [[] for _ in range(ver+1)]
+    for _ in range(e):
+        u, v = map(int, input().split())
+        g[u].append(v)
+        g[v].append(u)
+    return g
+
+def dfs(u, parent, g, subtree_size):
+    subtree_size[u] = 1   # count itself
+    for v in g[u]:
+        if v != parent:   # avoid going back to parent
+            dfs(v, u, g, subtree_size)
+            subtree_size[u] += subtree_size[v]  # add child subtree
+
+
+n, r = map(int, input().split())  
+graphh = graph(n, n-1)
+
+subtree_size = [0] * (n+1)
+
+dfs(r, -1, graphh, subtree_size)
+
+q = int(input())
+for _ in range(q):
+    x = int(input())
+    print(subtree_size[x])
