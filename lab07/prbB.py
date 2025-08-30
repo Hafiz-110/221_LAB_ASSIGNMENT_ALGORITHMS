af0003f9fb9c56ffe7a5a7ff2102d3918287bdc5
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-30 08:52:32 +0600

    Update prbB.py

--- lab07/prbB.py
+++ lab07/prbB.py
@@ -2,42 +2,40 @@ import sys, heapq
 input = sys.stdin.readline
 
 n, m, s, t = map(int, input().split())
-def graphw_weight(n, m):
+def garph(n, m):
     g = {i:[] for i in range(1, n+1)}
     for _ in range(m):
         u, v, w = map(int, input().split())
         g[u].append((v, w))
-
     return g
-g = graphw_weight(n, m)
+g = garph(n, m)
 
-def dijkstra(g, s, n):
-    dist = [float('inf') for _ in range(n+1)]
-    
+def dijkstra(g, n, s):
+    inf = float('inf')
+    dist = [inf for _ in range(n+1)]
 
     dist[s] = 0
-    pq = [(0, s)]
-    
+    pq = [(dist[s], s)]
+
     while pq:
-        curr_d, curr = heapq.heappop(pq)
-        if curr_d == dist[curr]:
-            for v, w in g[curr]:
-                if dist[curr]+w < dist[v]:
-                    dist[v] = dist[curr]+w
-                    
+        du, u = heapq.heappop(pq)
+        if du == dist[u]:
+            for v, w in g[u]:
+                if dist[v] > du+w:
+                    dist[v] = du+w
                     heapq.heappush(pq, (dist[v], v))
     return dist
 
-dist_s = dijkstra(g, s, n)
-dist_t = dijkstra(g, t, n)
+dist_a = dijkstra(g, n, s)
+dist_b = dijkstra(g, n, t)
 
-best_time = float('inf'); best_n = -1
+inf = float('inf')
+best_time = inf; best_n = -1
 for i in range(1, n+1):
-    if dist_s[i]!=float('inf') and dist_t[i]!=float('inf'):
-        meet_time = max(dist_s[i], dist_t[i])
+    if dist_a[i]!=inf and dist_b!=inf:
+        meet_time = max(dist_a[i], dist_b[i])
         if meet_time<best_time or (meet_time==best_time and i<best_n):
             best_time = meet_time
             best_n = i
-if best_n == -1: print(-1)
-else: print(best_time, best_n)
 
+print(-1 if best_n==-1 else f'{best_time} {best_n}')
