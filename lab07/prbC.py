fbef5d50b117276a3ddd134cc4314ca4ab3324c1
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-30 10:34:12 +0600

    Update prbC.py

--- lab07/prbC.py
+++ lab07/prbC.py
@@ -2,29 +2,30 @@ import sys, heapq
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-def graphw_weight(n, m):
+def graph(n, m):
     g = {i:[] for i in range(1, n+1)}
     for _ in range(m):
         u, v, w = map(int, input().split())
         g[u].append((v, w))
         g[v].append((u, w))
     return g
-g = graphw_weight(n, m)
+g = graph(n, m)
 
 def dijkstra(g, n):
-    danger = [float('inf') for _ in range(n+1)]
-    
+    inf = float('inf')
+    danger = [inf for _ in range(n+1)]
+
     danger[1] = 0
     pq = [(danger[1], 1)]
+
     while pq:
-        curr_d, curr = heapq.heappop(pq)
-        if curr_d == danger[curr]:
-            for v, w in g[curr]:
-                if danger[v] > max(danger[curr], w):
-                    danger[v] = max(danger[curr], w)
+        du, u = heapq.heappop(pq)
+        if du == danger[u]:
+            for v, w in g[u]:
+                if danger[v] > max(du, w):
+                    danger[v] = max(du, w)
                     heapq.heappush(pq, (danger[v], v))
     
-    for i in danger[1:]:
-        print(i if i != float('inf') else -1, end=' ')
+    print(*(-1 if i==inf else i for i in danger[1:]))
 
 dijkstra(g, n)
