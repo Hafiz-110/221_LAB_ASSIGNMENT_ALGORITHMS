881e577210e34b2eae33c9675c05c752624fcfa1
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-08-30 08:25:39 +0600

    reduced driver code. used exit

--- lab07/prbA.py
+++ lab07/prbA.py
@@ -1,10 +1,9 @@
 import sys, heapq
 input = sys.stdin.readline
 
-
 n, m, s, d = map(int, input().split())
 
-def graphw_weight(n, m):
+def graph(n, m):
     u = list(map(int, input().split()))
     v = list(map(int, input().split()))
     w = list(map(int, input().split()))
@@ -12,41 +11,35 @@ def graphw_weight(n, m):
     for i in range(m):
         g[u[i]].append((v[i], w[i]))
     return g
-
-g = graphw_weight(n, m)
+g = graph(n, m)
 
 def dijkstra(g, n, s, d):
     dist = [float('inf') for _ in range(n+1)]
-    p = [None for _ in range(n+1)]
+    parent = [None for _ in range(n+1)]
 
     dist[s] = 0
-    pq = [(0, s)]
+    pq = [(dist[s], s)]
 
     while pq:
-        dist_u, u = heapq.heappop(pq)
-        if u==d: break    # modification
-
-        if dist_u == dist[u]:
+        d_u, u = heapq.heappop(pq)
+        if u == d: break
+        if d_u == dist[u]:
             for v, w in g[u]:
-                if dist[u]+w < dist[v]:
-                    dist[v] = dist[u]+w
-                    p[v] = u
-                    heapq.heappush(pq, (dist[v], v))    # dijkstra algo done!
-
-    # path construction
-    if dist[d] == float('inf'): return -1, []
-
+                if dist[v] > d_u+w:
+                    dist[v] = d_u+w
+                    parent[v] = u
+                    heapq.heappush(pq, (dist[v], v))
+    
+    if dist[d] == float('inf'):
+        print(-1); exit()
+    
     path = []
-    curnt = d
-    while curnt:
-        path.append(curnt)
-        curnt = p[curnt]
+    curr = d
+    while curr:
+        path.append(curr)
+        curr = parent[curr]
+
+    print(dist[d])
     path.reverse()
-    return dist[d], path
-
-des_d, path = dijkstra(g, n, s, d)
-
-if des_d == -1: print(-1)
-else:
-    print(des_d)
     print(*path)
+dijkstra(g, n, s, d)
